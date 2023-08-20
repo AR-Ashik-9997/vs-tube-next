28a2bf4b6e762dd1740af5d3fa92e7678a53f100
AR-Ashik-9997 <ashikur.rahman9997@gmail.com>
2023-08-20 14:55:44 +0600

    update code

--- components/vmcard.tsx
+++ components/vmcard.tsx
@@ -21,18 +21,11 @@ const Vmcard = () => {
             src="/images/card-example-5.jpeg"
           />
           <CardFooter className="absolute bg-black/40 bottom-0 z-10  border-t-1 border-default-600 dark:border-default-100">
-            <div className="flex flex-grow gap-2 items-center">
-              <Image
-                alt="Breathing app icon"
-                className="rounded-full w-10 h-11 bg-black"
-                src="/images/breathing-app-icon.jpeg"
-              />
-              <div>
-                <p className="text-tiny text-white/60">Breathing App</p>
-                <p className="text-tiny text-white/60">
-                  Get a good night's sleep.
-                </p>
-              </div>
+            <div>
+              <p className="text-tiny text-white/60">Breathing App</p>
+              <p className="text-tiny text-white/60">
+                Get a good night's sleep.
+              </p>
             </div>
           </CardFooter>
         </Card>
--- pages/[watch].tsx
+++ pages/[watch].tsx
@@ -1,6 +1,7 @@
 import React, { ReactElement } from "react";
 import Layout from "@/layouts/default";
 import PlayList from "@/components/playlist";
+
 const Watch = () => {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-12 gap-4 w-4/5 sm:mx-auto pt-12">
@@ -15,7 +16,7 @@ const Watch = () => {
         </div>
       </div>
       <div className="lg:col-span-4">
-        <div className="h-96 lg:h-80 xl:h-96 2xl:h-[34rem] border rounded-xl p-4 overflow-auto">
+        <div className="max-h-96 lg:max-h-80 xl:max-h-96 2xl:max-h-[34rem] border rounded-xl p-4 overflow-y-scroll outline-none">
           <PlayList />          
           <PlayList />          
           <PlayList />          
