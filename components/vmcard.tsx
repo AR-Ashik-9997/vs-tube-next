3e0b127e9e1fc62b88e75b3f9c4cf62d1f265fd5
AR-Ashik-9997 <ashikur.rahman9997@gmail.com>
2023-08-22 03:12:57 +0600

    text add under the video

--- components/vmcard.tsx
+++ components/vmcard.tsx
@@ -4,17 +4,18 @@ import Link from "next/link";
 
 const Vmcard = ({ AllData }: IData) => {
   return (
-    <div className="sm:gap-4 xl:gap-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  xl:grid-cols-4  w-4/5 sm:mx-auto pt-12">
+    <div className="sm:gap-4 xl:gap-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  xl:grid-cols-4  w-4/5 sm:mx-auto pt-12">
       {AllData?.data.map((item: IPlayList) => (
         <Link href={`/watches/${item.id}`} key={item?.id}>
-          <Card className="w-full h-[300px] mb-4">
+          <Card className="w-full h-auto mb-4">
             <Image
               removeWrapper
               alt="Relaxing app background"
-              className="z-0 w-full h-full"
+              className="z-0 w-full h-auto"
               src={item?.image}
             />
           </Card>
+          <h1 className="text-xs xl:text-lg mb-4">{item?.title}</h1>
         </Link>
       ))}
     </div>
