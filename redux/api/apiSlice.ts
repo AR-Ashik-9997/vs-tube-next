e875acf6ab6f3d227718b3a5e42e4ddbc0d7d70b
AR-Ashik-9997 <ashikur.rahman9997@gmail.com>
2023-10-25 00:44:00 +0600

    update

--- redux/api/apiSlice.ts
+++ redux/api/apiSlice.ts
@@ -3,7 +3,7 @@ import { createApi } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   reducerPath: "api",
   baseQuery: axiosBaseQuery({
-    baseUrl: `${process.env.DB_HOST}`,
+    baseUrl: "https://vs-tube-server-pk04ada77-ar-ashik-9997.vercel.app/api/v1",
   }),
   tagTypes: ["All"],
   endpoints: () => ({}),
